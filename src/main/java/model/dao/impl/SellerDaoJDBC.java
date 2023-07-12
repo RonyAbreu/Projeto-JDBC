4f314c711083c407a152041e93bcca7ba8829dab
RonyAbreu <ronyelison78@gmail.com>
2023-07-12 17:09:11 -0300

    implementação do método findById da classe SellerDaoJDBC

--- src/main/java/model/dao/impl/SellerDaoJDBC.java
+++ src/main/java/model/dao/impl/SellerDaoJDBC.java
@@ -1,11 +1,24 @@
 package model.dao.impl;
 
+import db.DB;
+import db.DbException;
+import entities.Department;
 import entities.Seller;
 import model.dao.SellerDao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class SellerDaoJDBC implements SellerDao {
+    private Connection conn;
+
+    public SellerDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
+
     @Override
     public void insertSeller(Seller s) {
 
@@ -23,7 +36,35 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public Seller findById(Integer id) {
-        return null;
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try{
+            st = conn.prepareStatement("select seller.*,department.Name as DepName from seller inner join department on seller.DepartmentId = department.Id where seller.Id = ?");
+            st.setInt(1, id);
+
+            rs = st.executeQuery();
+            if (rs.next()){
+                Department dep = new Department();
+                dep.setId(rs.getInt("DepartmentId"));
+                dep.setName(rs.getString("DepName"));
+
+                Seller sel = new Seller();
+                sel.setId(rs.getInt("Id"));
+                sel.setName(rs.getString("Name"));
+                sel.setEmail(rs.getString("Email"));
+                sel.setBirthDate(rs.getDate("BirthDate"));
+                sel.setBaseSalary(rs.getDouble("BaseSalary"));
+                sel.setDepartment(dep);
+
+                return sel;
+            }
+            return null;
+        } catch (SQLException e){
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
     }
 
     @Override
