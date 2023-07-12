32cdb328dd8ced0f84597278f28d5cfbc7a4c21f
RonyAbreu <ronyelison78@gmail.com>
2023-07-12 17:10:32 -0300

    alterações na classe Program

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,11 +1,8 @@
 package application;
 
-import entities.Department;
 import entities.Seller;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-
-import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
